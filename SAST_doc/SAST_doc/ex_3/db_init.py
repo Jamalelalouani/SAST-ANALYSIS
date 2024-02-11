5a512523e0c04255aa0e54f07f63b79ec64683d2
Jamalelalouani <95922790+Jamalelalouani@users.noreply.github.com>
2024-02-11 16:44:29 +0100

    Update db_init.py

--- SAST_doc/SAST_doc/ex_3/db_init.py
+++ SAST_doc/SAST_doc/ex_3/db_init.py
@@ -3,7 +3,6 @@
 import os
 import sqlite3
 
-
 def db_init_users():
 
     users = [
@@ -14,24 +13,22 @@ def db_init_users():
 
     conn = sqlite3.connect('db_users.sqlite')
     c = conn.cursor()
-    c.execute("CREATE TABLE users (username text, password text, failures int, mfa_enabled int, mfa_secret text)")
+    c.execute("CREATE TABLE IF NOT EXISTS users (username text, password text, failures int, mfa_enabled int, mfa_secret text)")
 
-    for u,p in users:
-        c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(u, p, 0, 0, ''))
+    for u, p in users:
+        c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES (?, ?, ?, ?, ?)", (u, p, 0, 0, ''))
 
     conn.commit()
     conn.close()
-
 
 def db_init_posts():
 
     conn = sqlite3.connect('db_posts.sqlite')
     c = conn.cursor()
-    c.execute("CREATE TABLE posts (date date, username text, text text)")
+    c.execute("CREATE TABLE IF NOT EXISTS posts (date date, username text, text text)")
 
     conn.commit()
     conn.close()
-
 
 if __name__ == '__main__':
 

ae34c8082c466490923bfcc4686c63f570ea681a
Jamalelalouani <95922790+Jamalelalouani@users.noreply.github.com>
2024-02-11 16:50:32 +0100

    Update vulpy-ssl.py

--- SAST_doc/SAST_doc/ex_3/vulpy-ssl.py
+++ SAST_doc/SAST_doc/ex_3/vulpy-ssl.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 
 from flask import Flask, g, redirect, request
-
 from mod_hello import mod_hello
 from mod_user import mod_user
 from mod_posts import mod_posts
 from mod_mfa import mod_mfa
-
 import libsession
+import os
 
 app = Flask('vulpy')
-app.config['SECRET_KEY'] = 'aaaaaaa'
 
+# Configuration de la clé secrète
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback_secret_key')
+
+# Enregistrement des blueprints
 app.register_blueprint(mod_hello, url_prefix='/hello')
 app.register_blueprint(mod_user, url_prefix='/user')
 app.register_blueprint(mod_posts, url_prefix='/posts')
 app.register_blueprint(mod_mfa, url_prefix='/mfa')
 
-
+# Route par défaut
 @app.route('/')
 def do_home():
     return redirect('/posts')
 
+# Middleware de session
 @app.before_request
 def before_request():
     g.session = libsession.load(request)
 
+# Exécution de l'application avec SSL
 app.run(debug=True, host='127.0.1.1', ssl_context=('/tmp/acme.cert', '/tmp/acme.key'))
